056c72ee95551bc81829e54583d08bef0fe4adf6
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-03 23:01:17 +0900

    shoppingmall : javascript

--- shoppingmall_practice/src/main.js
+++ shoppingmall_practice/src/main.js
@@ -7,13 +7,14 @@ function loadItems() {
     .then(json => json.items); // json의 items만 받아온다
 }
 
+// items는 json 형태이므로 html 폼으로 바꿔준다
+// 받아온 아이템을 ul items에 li item으로서 추가한다
 function displayItems (items) {
-    // items는 json 형태이므로 html 폼으로 바꿔준다
-    // 받아온 아이템을 ul items에 li item으로서 추가한다
     const ul = document.querySelector('.items');
     ul.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// 받은 data item으로 list를 반환
 function createHTMLString(item) {
     return `
     <li class="item">
@@ -29,7 +30,28 @@ function createHTMLString(item) {
 loadItems() //item들을 동적으로 받아와서
 .then(items => { //promise가 성공적으로 받아오면 전달받은 items들을 이용
     console.log(items);
-     displayItems(items); // item 보여주기
-    // setEventListner(items) // item 필터링
+    displayItems(items); // item 보여주기
+    
+    setEventListner(items) // item 필터링
 })
-.catch(console.log); 
+.catch(console.log);
+// 1. 배열을 두 개 만든다 - alt를 이용할 수 있는 것과 색상을 이용해야 하는 것
+// 2. 버튼을 누르면 alt 값과 비교해서 그것과 일치하는 li만 가져오게 하는 함수 정의한다
+
+
+function setEventListner(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelectorAll('.buttons');
+    logo.addEventListener('click', () => {
+        displayItems(items);
+    });
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+function onButtonClick(event, items) {
+    console.log(event);
+
+
+
+
+}
