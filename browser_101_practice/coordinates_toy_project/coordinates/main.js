f1def2dc9f077a01b3b64ec564cab35674098e9d
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-14 13:55:09 +0900

    browser : optimizing coordinates toy project
    top, left 대신 translate()을 사용해서 layer 줄이기

--- browser_101_practice/coordinates_toy_project/coordinates/main.js
+++ browser_101_practice/coordinates_toy_project/coordinates/main.js
@@ -4,6 +4,9 @@ const target = document.querySelector('.target');
 const result = document.querySelector('.result');
 const vertical = document.querySelector('.line.vertical');
 const horizontal = document.querySelector('.line.horizontal');
+const targetRect = target.getBoundingClientRect();
+const targetWidth = targetRect.width / 2;
+const targetHeight = targetRect.height / 2;
 
 document.addEventListener('mousemove', e => {
     // result에 동적으로 좌표를 출력하게 만든다
@@ -11,17 +14,15 @@ document.addEventListener('mousemove', e => {
     let X = e.clientX;
 
     result.innerHTML = `🚩X 좌표 : ${X} 🚩Y 좌표 : ${Y}`;
-    result.style.top = `${Y}px`;
-    result.style.left = `${X}px`;
-    
+
+    result.style.transform = `translate(${X}px, ${Y}px)`;
 
     // vertical, horizontal line 을 동적으로 움직이게 만든다
-    vertical.style.left = `${X}px`;
-    horizontal.style.top = `${Y}px`;
+    vertical.style.transform = `translateX(${X}px)`;
+    horizontal.style.transform = `translateY(${Y}px)`;
 
     // target image도 동적으로 움직이게 만든다
-    target.style.left = `${X}px`;
-    target.style.top = `${Y}px`;
+    target.style.transform = `translate(${X - targetWidth}px, ${Y - targetHeight}px)`;
 });
 
 // 추가할 기능
