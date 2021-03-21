46f74d212e5708812dbd47195d81801c215f8683
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-21 18:30:56 +0900

    GAME : CSS styling

---
+++ browser_101_practice/game_practice/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const playBbtn = document.querySelector('.play__btn');
+const restartBtn = document.querySelector('.restart__btn');
+const timer = document.querySelector('.timer');
+const counter = document.querySelector('.counter');
+// Start play Btn
