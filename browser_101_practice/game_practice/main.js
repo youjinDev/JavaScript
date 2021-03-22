9494c7cf3f11c5af4167da56fb9aa6b37feccd01
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-22 21:21:38 +0900

    GAME : js
    add click event

--- browser_101_practice/game_practice/main.js
+++ browser_101_practice/game_practice/main.js
@@ -6,11 +6,15 @@ const restartBtn = document.querySelector('.restart__btn');
 const counter = document.querySelector('.counter');
 const wrapper = document.querySelector('.car__wrapper');
 const popup = document.querySelector('.popup');
+let timerDisplay = document.querySelector('.timer');
 
 // Start play button
 playBtn.addEventListener('click', settingGame);
 
 function settingGame() {
+
+    // 0. 버튼 비활성화
+    // playBtn.disabled = true;
 
     // 1. 자동차 추가
     wrapper.innerHTML = `
@@ -50,40 +54,74 @@ function settingGame() {
         randomPlace(currentValue)
     });
 
-} // settingGame함수 끝
-
+    
+}
+// settingGame함수 끝
 
 
 // 3. 타이머 시작
-let timerDisplay = document.querySelector('.timer');
-ltimerDisplay.innerHTML = 10;
+let setTimer;
 
 playBtn.addEventListener('click', () => {
-    let countdown = setInterval(() => {
+
+    counter.innerHTML = 10;
+
+    // playBtn 클릭 방지
+    playBtn.disabled = true;
+
+    // timer 시작
+    timerDisplay.innerHTML = 10;
+    setTimer = setInterval(() => {
         timerDisplay.innerHTML = timerDisplay.innerHTML - 1;
+
         if (timerDisplay.innerHTML == 0) {
-            gameover();
-            clearInterval(countdown);
+            gameoverPop();
         }
     }, 1000);
-})
 
-    // gameover 종류 = 초록차 눌렀을 때 or 시간 다 됐을때
-function gameover() {
+    wrapper.addEventListener('click', (e) => {
+        (e.target.className === 'car car__green') && gameoverPop();
+        counter.innerHTML -= 1;
+        removeCar(e);
+        if (counter.innerHTML == 0) {
+            stopTimer();
+            alert('you win!!');
+        }
+        
+    });
+
+
+
+
+
+
+
+
+
+
+    
+});
+
+
+// gameover 종류 = 초록차 눌렀을 때 or 시간 다 됐을때
+function gameoverPop() {
     // 팝업 띄우기
-    popup.classList.remove('invisible');
+    popup.classList.toggle('invisible');
+    playBtn.classList.toggle('invisible');
+
+    //timer 정지
+    stopTimer();
+
+    restartBtn.addEventListener('click', () => {
+        console.log('fuck');
+    })
+
 }
 
+function stopTimer() {
+    clearInterval(setTimer);
+}
 
-
-// 3. 남은 갯수를 표시한다
-// 4. 다른 것을 클릭하면 게임을 종료한다
-
-
-
-
-
-
-
-
-
+function removeCar(e) {
+    wrapper.removeChild(e.target);
+}
