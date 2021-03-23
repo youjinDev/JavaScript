4aa8cfea8e9bb00979a7c1cd841b5f1fcaa839b4
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-23 15:48:30 +0900

    GAME : js
    restart button event, function

--- browser_101_practice/game_practice/main.js
+++ browser_101_practice/game_practice/main.js
@@ -7,17 +7,61 @@ const counter = document.querySelector('.counter');
 const wrapper = document.querySelector('.car__wrapper');
 const popup = document.querySelector('.popup');
 let timerDisplay = document.querySelector('.timer');
+const p = document.querySelector('.p');
 
 // Start play button
 playBtn.addEventListener('click', settingGame);
 
-function settingGame() {
+// 타이머 시작
+let setTimer;
+let isPause = false;
 
-    // 0. 버튼 비활성화
-    // playBtn.disabled = true;
+playBtn.addEventListener('click', () => {
+
+    // counter.innerHTML = 10;
+    // timerDisplay.innerHTML = 10;
+
+    // playBtn 클릭 방지
+    playBtn.disabled = true;
+
+    // timer 시작
+    startTimer();
+
+
+    wrapper.addEventListener('click', (e) => {
+
+        console.log(e.target.className);
+
+        if (e.target.className === 'car car__green' || e.target.className ==='car car__truck') {
+            p.innerHTML = 'LOSE🤬'
+            gameoverPop();
+            return;
+        }
+        counter.innerHTML -= 1;
+        removeCar(e);
+
+        // 게임 성공했을 때
+        if (counter.innerHTML == 0) {
+            p.innerHTML = 'WIN🤩'
+            stopTimer();
+            gameoverPop();
+        }
+        
+    });
+});
+
+
+// function
+
+function settingGame() {
 
     // 1. 자동차 추가
     wrapper.innerHTML = `
+        <img src="carrot/img/car--green.png" alt="" class="car car__green">
+        <img src="carrot/img/car--green.png" alt="" class="car car__green">
+        <img src="carrot/img/car--green.png" alt="" class="car car__green">
+        <img src="carrot/img/car--green.png" alt="" class="car car__green">
+        <img src="carrot/img/car--green.png" alt="" class="car car__green">
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
@@ -28,16 +72,11 @@ function settingGame() {
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
         <img src="carrot/img/car--red.png" alt="" class="car car__red">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
-        <img src="carrot/img/car--green.png" alt="" class="car car__green">
+        <img src="carrot/img/car--truck.png" alt="" class="car car__truck">
+        <img src="carrot/img/car--truck.png" alt="" class="car car__truck">
+        <img src="carrot/img/car--truck.png" alt="" class="car car__truck">
+        <img src="carrot/img/car--truck.png" alt="" class="car car__truck">
+        <img src="carrot/img/car--truck.png" alt="" class="car car__truck">
     `;
 
     // 2. 자동차 랜덤 배치
@@ -53,55 +92,7 @@ function settingGame() {
     cars.forEach((currentValue) => {
         randomPlace(currentValue)
     });
-
-    
 }
-// settingGame함수 끝
-
-
-// 3. 타이머 시작
-let setTimer;
-
-playBtn.addEventListener('click', () => {
-
-    counter.innerHTML = 10;
-
-    // playBtn 클릭 방지
-    playBtn.disabled = true;
-
-    // timer 시작
-    timerDisplay.innerHTML = 10;
-    setTimer = setInterval(() => {
-        timerDisplay.innerHTML = timerDisplay.innerHTML - 1;
-
-        if (timerDisplay.innerHTML == 0) {
-            gameoverPop();
-        }
-    }, 1000);
-
-    wrapper.addEventListener('click', (e) => {
-        (e.target.className === 'car car__green') && gameoverPop();
-        counter.innerHTML -= 1;
-        removeCar(e);
-        if (counter.innerHTML == 0) {
-            stopTimer();
-            alert('you win!!');
-        }
-        
-    });
-
-
-
-
-
-
-
-
-
-
-    
-});
-
 
 // gameover 종류 = 초록차 눌렀을 때 or 시간 다 됐을때
 function gameoverPop() {
@@ -111,15 +102,41 @@ function gameoverPop() {
 
     //timer 정지
     stopTimer();
+}
 
-    restartBtn.addEventListener('click', () => {
-        console.log('fuck');
-    })
+// restart btn 누르면
+restartBtn.addEventListener('click', () => {
+    popup.classList.toggle('invisible');
+    playBtn.classList.toggle('invisible');
+    startTimer();
+    settingGame();
+})
+
+function startTimer() {
+    // 변수 초기화
+    isPause = false;
+    counter.innerHTML = 10;
+    timerDisplay.innerHTML = 10;
+
+    setTimer = setInterval(() => {
+        if(!isPause) {
+        timerDisplay.innerHTML = timerDisplay.innerHTML - 1;
+
+        if (timerDisplay.innerHTML == 0) {
+            p.innerHTML = 'TIME OUT😰'
+            gameoverPop();
+            // return;
+        }
+    }
+
+    }, 1000);
 
 }
 
 function stopTimer() {
     clearInterval(setTimer);
+    setTimer = null;
+    isPause = true;
 }
 
 function removeCar(e) {
