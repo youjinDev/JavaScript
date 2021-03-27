3a5fde25dff2d0b4ede3972773c991f8ea2ae09b
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-27 20:16:40 +0900

    GAME : field class 분리

---
+++ game_project/src/field.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const IMG_SIZE = 100;
+const right_car_sound = new Audio('carrot/sound/carrot_pull.mp3');
+
+export default class Field {
+    constructor(redCarCount, greenCarCount, truckCount) {
+        this.redCarCount = redCarCount;
+        this.greenCarCount = greenCarCount;
+        this.truckCarCount = truckCount;
+
+        this.field = document.querySelector('.game__field');
+        this.fieldRect = this.field.getBoundingClientRect();
+        // this binding을 통해 onclick이 호출되었을 때, 그 안의 onItemClick의 this가 클래스를 가리킨다고 알려줘야함
+        this.field.addEventListener('click', (event) => this.onClick(event));
+    }
+
+    init() {
+        this.field.innerHTML = '';
+        this._createItems('car green', this.greenCarCount, './carrot/img/car--green.png');
+        this._createItems('car red', this.redCarCount, './carrot/img/car--red.png');
+        this._createItems('car truck', this.truckCarCount, './carrot/img/car--truck.png');
+    }
+
+    setClickListener(onItemClick) {
+        this.onItemClick = onItemClick;
+    }
+
+    _createItems(className, count, imgPath) {
+        const x1 = 0;
+        const y1 = 0;
+        const x2 = this.fieldRect.width - IMG_SIZE;
+        const y2 = this.fieldRect.height - IMG_SIZE;
+        
+        for (let i = 0; i < count ; i++) {
+            const item = document.createElement('img');
+    
+            item.setAttribute('class', className);
+            item.setAttribute('src', imgPath);
+    
+            item.style.width = `${IMG_SIZE}px`;
+            item.style.height = `${IMG_SIZE}px`;
+            item.style.position = 'absolute';
+            item.style.cursor = 'pointer';
+    
+            const x = randomNum(x1, x2);
+            const y = randomNum(y1, y2);
+    
+            item.style.left = `${x}px`;
+            item.style.top = `${y}px`;
+    
+            this.field.appendChild(item);
+        }
+    }
+
+    onClick(event) {
+        const target = event.target;
+        if (target.matches('.car.red')) {
+            playSound(right_car_sound);
+            target.remove();
+            this.onItemClick && this.onItemClick('carRight');
+        } else if (target.matches('.car.green') || target.matches('.car.truck')) {
+            this.onItemClick && this.onItemClick('carWrong');
+        }
+    }
+}
+
+function randomNum (min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
+}
--- game_project/src/main.js
+++ game_project/src/main.js
@@ -1,25 +1,19 @@
 'use strict';
 import PopUp from './popup.js';
+import Field from './field.js';
+
 
 const GREEN_CAR_COUNT = 10;
 const RED_CAR_COUNT = 5;
 const TRUCK_COUNT = 10;
-const IMG_SIZE = 100; // 100*100
 const TIME = 5;
 
 const alert_sound = new Audio('carrot/sound/alert.wav');
 const back_sound = new Audio('carrot/sound/bg.mp3');
-const right_car_sound = new Audio('carrot/sound/carrot_pull.mp3');
 const win_sound = new Audio('carrot/sound/game_win.mp3');
 
-const field = document.querySelector('.game__field');
-const fieldRect = field.getBoundingClientRect();
 const playBtn = document.querySelector('.play__btn');
-// const restartBtn = document.querySelector('.restart__btn');
 const counter = document.querySelector('.game__counter');
-
-// const popup = document.querySelector('.popup');
-// const message = document.querySelector('.message');
 
 const timerDisplay = document.querySelector('.game__timer');
 const btnImg = document.querySelector('.play__btn>.fas');
@@ -29,36 +23,51 @@ let score = 0;
 let setTimer = undefined;
 
 const gameFinishBanner = new PopUp();
+gameFinishBanner.setClickListener(startGame);
 
-gameFinishBanner.setClickListener(() => {
-    initGame();
-})
+const gameField = new Field(RED_CAR_COUNT, GREEN_CAR_COUNT, TRUCK_COUNT);
+gameField.setClickListener(onItemClick);
 
-// Start play button
+function onItemClick(itemType) {
+    console.log(itemType);
+
+    if(!started) {
+        return;
+    }
+
+    if (itemType === 'carRight') {
+        score++;
+        counter.innerHTML = RED_CAR_COUNT - score;
+        if (score === RED_CAR_COUNT) {
+            finishGame(true);
+        }
+    } else if (itemType === 'carWrong') {
+        finishGame(false);
+        return;
+    }
+}
+
 playBtn.addEventListener('click', () => {
-    
     if (started) {
         stopGame();
     } else {
         startGame();
     }
-
 });
 
+
 function stopGame() {
-
     started = false;
-    finishGame(false);
-    stopGameTimer(setTimer);
-
+    stopSound(back_sound);
+    hideStartButton();
+    gameFinishBanner.showWithText(' ');
+    stopGameTimer();
 }
 
 function startGame() {
     started = true;
-
+    playSound(back_sound);
     initGame();
-
-    // 버튼 이미지 바꾸기
     changeBtnImg();
 }
 
@@ -72,23 +81,8 @@ function stopSound(sound) {
 }
 
 function initGame() {
-
+    gameField.init();
     showStartButton();
-
-    started = true;
-
-    //bgm 켜기
-    playSound(back_sound);
-
-    //필드 초기화
-    field.innerHTML = '';
-
-    // 아이템 추가
-    createItems('car green', GREEN_CAR_COUNT, './carrot/img/car--green.png');
-    createItems('car red', RED_CAR_COUNT, './carrot/img/car--red.png');
-    createItems('car truck', TRUCK_COUNT, './carrot/img/car--truck.png');
-    
-    // 카운터, 타이머 초기화
     resetCounter();
     startTimer();
 }
@@ -98,36 +92,6 @@ function changeBtnImg() {
     btnImg.classList.remove('fa-play');
 }
 
-function createItems(className, count, imgPath) {
-    const x1 = 0;
-    const y1 = 0;
-    const x2 = fieldRect.width - IMG_SIZE;
-    const y2 = fieldRect.height - IMG_SIZE;
-    
-    for (let i = 0; i < count ; i++) {
-        const item = document.createElement('img');
-
-        item.setAttribute('class', className);
-        item.setAttribute('src', imgPath);
-
-        item.style.width = `${IMG_SIZE}px`;
-        item.style.height = `${IMG_SIZE}px`;
-        item.style.position = 'absolute';
-        item.style.cursor = 'pointer';
-
-        const x = randomNum(x1, x2);
-        const y = randomNum(y1, y2);
-
-        item.style.left = `${x}px`;
-        item.style.top = `${y}px`;
-
-        field.appendChild(item);
-    }
-}
-
-function randomNum (min, max) {
-    return Math.random() * (max - min) + min;
-}
 
 function resetCounter() {
     score = 0;
@@ -143,7 +107,6 @@ function startTimer() {
         timerDisplay.innerHTML = `0:${remainTime}`;
 
         if (remainTime === 0 ) {
-
             finishGame(false);
             playSound(alert_sound);
             return;
@@ -151,17 +114,14 @@ function startTimer() {
     }, 1000)
 }
 
-function stopGameTimer(timer) {
-    clearInterval(timer);
-    stopSound(back_sound);
+function stopGameTimer() {
+    clearInterval(setTimer);
 }
 
 function finishGame(win) {
-
     started = false;
-    hideStartButton();
-    stopGameTimer(setTimer);
-
+    stopGameTimer();
+    stopSound(back_sound);
     if (win) {
         gameFinishBanner.showWithText('YOU WON🤩');
         playSound(win_sound);
@@ -179,21 +139,5 @@ function showStartButton() {
     playBtn.style.visibility = 'visible';
 }
 
-field.addEventListener('click', (e) => {
-    const target = e.target;
-    
-    if (target.className === 'car green' || target.className === 'car truck') {
-        finishGame(false);
-        return;
-    } else if (target.className === 'car red') {
-    target.remove();
-    playSound(right_car_sound);
-    score++;
-    counter.innerHTML = RED_CAR_COUNT - score;
-    }
-    
-    if (score === RED_CAR_COUNT) {
-        finishGame(true);
-    }
-});
+
 
