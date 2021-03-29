09b6a76d2bb0e7550d1d3862f917909433bd2a61
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-03-29 15:03:40 +0900

    GAME : game class 분리

--- game_project/src/game.js
+++ game_project/src/game.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import Field from './field.js';
+import * as sound from './sound.js'
+
 export default class Game {
     constructor(gameDuration, redCarCount, greenCarCount, truckCount) {
         this.gameDuration = gameDuration;
@@ -9,11 +12,124 @@ export default class Game {
 
         this.counter = document.querySelector('.game__counter');
         this.timerDisplay = document.querySelector('.game__timer');
-        this.playBtn = document.querySelector('.play__btn');
-        
         this.btnImg = document.querySelector('.play__btn>.fas');
+        this.playBtn = document.querySelector('.play__btn');
+
+        this.playBtn.addEventListener('click', () => {
+            if (this.started) {
+                this.stop();
+            } else {
+                this.start();
+            }
+        });
+
+        this.gameField = new Field(redCarCount, greenCarCount, truckCount);
+        this.gameField.setClickListener(this.onItemClick);
+
         this.score = 0;
         this.started = false;
         this.setTimer = undefined;
     }
+
+    setGameStopListener(onGameStop) {
+        this.onGameStop = onGameStop;
+    }
+
+    init() {
+        this.gameField.init();
+        this.showStartButton();
+        this.resetCounter();
+        this.startTimer();
+    }
+
+    start() {
+        this.started = true;
+        this.init();
+        this.changeBtnImg();
+        sound.playBackground();
+    }
+
+    stop() {
+        this.started = false;
+        this.hideStartButton();
+        this.stopGameTimer();
+        sound.stopBackground();
+        this.onGameStop && this.onGameStop('cancle');
+    }
+
+    finish(win) {
+        this.started = false;
+        this.stopGameTimer();
+        sound.stopBackground();
+        if (win) {
+            sound.playWin();
+        } else {
+            sound.playAlert();
+        }
+        this.onGameStop && this.onGameStop(win? 'win' : 'lose');
+
+    }
+    
+    onItemClick = itemType => {
+        if(!this.started) {
+            return;
+        }
+    
+        if (itemType === 'carRight') {
+            this.score++;
+            this.counter.innerHTML = this.redCarCount - this.score;
+            if (this.score === this.redCarCount) {
+                this.finish(true);
+            }
+        } else if (itemType === 'carWrong') {
+            this.finish(false);
+            return;
+        }
+    }
+
+    hideStartButton() {
+        this.playBtn.style.visibility = 'hidden';
+    }
+    
+    showStartButton() {
+        this.playBtn.style.visibility = 'visible';
+    }
+    
+    changeBtnImg() {
+        this.btnImg.classList.add('fa-pause');
+        this.btnImg.classList.remove('fa-play');
+    }
+    
+    resetCounter() {
+        this.score = 0;
+        this.counter.innerHTML = this.redCarCount;
+    }
+    
+    startTimer() {
+        let remainTime = this.gameDuration;
+        this.timerDisplay.innerHTML = `0:${remainTime}`;
+        
+        this.setTimer = setInterval(() => {
+            --remainTime;
+            this.timerDisplay.innerHTML = `0:${remainTime}`;
+    
+            if (remainTime === 0 ) {
+                this.finish(false);
+                sound.playAlert();
+                return;
+                }
+        }, 1000)
+    }
+    
+    stopGameTimer() {
+        clearInterval(this.setTimer);
+    }
+
+    
+
+
+
+
+
+
 }
--- game_project/src/main.js
+++ game_project/src/main.js
@@ -1,130 +1,35 @@
 'use strict';
 
+import Game from './game.js';
 import PopUp from './popup.js';
-import Field from './field.js';
-import * as sound from './sound.js'
 
 const GREEN_CAR_COUNT = 10;
 const RED_CAR_COUNT = 5;
 const TRUCK_COUNT = 5;
 const TIME = 5;
 
-const playBtn = document.querySelector('.play__btn');
-const counter = document.querySelector('.game__counter');
-
-const timerDisplay = document.querySelector('.game__timer');
-const btnImg = document.querySelector('.play__btn>.fas');
-
-let started = false;
-let score = 0;
-let setTimer = undefined;
-
 const gameFinishBanner = new PopUp();
-gameFinishBanner.setClickListener(startGame);
-
-const gameField = new Field(RED_CAR_COUNT, GREEN_CAR_COUNT, TRUCK_COUNT);
-gameField.setClickListener(onItemClick);
-
-function onItemClick(itemType) {
-
-    if(!started) {
-        return;
-    }
-
-    if (itemType === 'carRight') {
-        score++;
-        counter.innerHTML = RED_CAR_COUNT - score;
-        if (score === RED_CAR_COUNT) {
-            finishGame(true);
-        }
-    } else if (itemType === 'carWrong') {
-        finishGame(false);
-        return;
-    }
-}
-
-playBtn.addEventListener('click', () => {
-    if (started) {
-        stopGame();
-    } else {
-        startGame();
-    }
+gameFinishBanner.setClickListener(() => {
+    game.start();
 });
 
-
-function stopGame() {
-    started = false;
-    sound.stopBackground();
-    hideStartButton();
-    gameFinishBanner.showWithText(' ');
-    stopGameTimer();
-}
-
-function startGame() {
-    started = true;
-    sound.playBackground();
-    initGame();
-    changeBtnImg();
-}
-
-function initGame() {
-    gameField.init();
-    showStartButton();
-    resetCounter();
-    startTimer();
-}
-
-function changeBtnImg() {
-    btnImg.classList.add('fa-pause');
-    btnImg.classList.remove('fa-play');
-}
-
-
-function resetCounter() {
-    score = 0;
-    counter.innerHTML = RED_CAR_COUNT;
-}
-
-function startTimer() {
-    let remainTime = TIME;
-    timerDisplay.innerHTML = `0:${remainTime}`;
-    
-    setTimer = setInterval(() => {
-        --remainTime;
-        timerDisplay.innerHTML = `0:${remainTime}`;
-
-        if (remainTime === 0 ) {
-            finishGame(false);
-            sound.playAlert();
-            return;
-            }
-    }, 1000)
-}
-
-function stopGameTimer() {
-    clearInterval(setTimer);
-}
-
-function finishGame(win) {
-    started = false;
-    stopGameTimer();
-    sound.stopBackground();
-    if (win) {
-        gameFinishBanner.showWithText('YOU WON🤩');
-        sound.playWin();
-    } else {
-        gameFinishBanner.showWithText('YOU LOSE🤬');
-        sound.playAlert();
+const game = new Game(TIME, RED_CAR_COUNT, GREEN_CAR_COUNT, TRUCK_COUNT);
+game.setGameStopListener((reason) => {
+    let message;
+    switch(reason) {
+        case 'win':
+            message = '이겼습니다!🙌';
+            break;
+        case 'cancle':
+            message = ' ';
+            break;
+        case 'lose':
+            message = '졌습니다!🤬'
+            break;
     }
-}
 
-function hideStartButton() {
-    playBtn.style.visibility = 'hidden';
-}
+    gameFinishBanner.showWithText(message);
 
-function showStartButton() {
-    playBtn.style.visibility = 'visible';
-}
-
+});
 
 
