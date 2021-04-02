c8a97c22c400d38915b698732dc3e811255eb274
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-04-02 17:16:58 +0900

    Request animation frame practice

--- javascript_practice/RFA.js
+++ javascript_practice/RFA.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // RAF는 rendering phase 진입 이전에 인자로 주어진 callback 수행을 보장해준다
+// RAF의 callback들은 RFA queue에 별도로 들어간다
+// click listenter를 click할 때 RAF callback을 수행하는 것이 아니라, 일단 RAF queue에 등록해놨다가 나중에 브라우저가 업데이트 될 때 변경 사항들을 적용하는 것
 
 let X = 0;
 let Y = 0;
@@ -18,8 +20,8 @@ rocket.addEventListener('click', () => {
             console.log('after first fly!');
         });
 
-
         requestAnimationFrame(() => {
+            // document.body.style.backgroundColor = 'red';
             console.log('second RAF!');
         });
 
@@ -27,9 +29,9 @@ rocket.addEventListener('click', () => {
             console.log('Last setTimeOut!');
         }, 0);
 
-
         } else {
             console.log('stopped');
+            document.body.style.backgroundColor = 'orange';
             cancelAnimationFrame(myReq);
             fired = !fired;
         }
@@ -37,9 +39,13 @@ rocket.addEventListener('click', () => {
 
 
 function fly() {
+    document.body.style.backgroundColor = 'skyBlue';
     X += 10;
     Y += 5;
     rocket.style.transform = `translate(${X}px, -${Y}px)`;
+    if (X < 1500) {
     myReq = requestAnimationFrame(fly);
+    }
 };
+
 
