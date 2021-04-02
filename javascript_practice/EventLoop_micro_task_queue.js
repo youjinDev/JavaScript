6345dfe0a84a6c77b46c5c2d15dbd73ef7935170
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-04-02 14:19:58 +0900

    Update EventLoop_micro_task_queue.js

--- javascript_practice/EventLoop_micro_task_queue.js
+++ javascript_practice/EventLoop_micro_task_queue.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// event loop는 micro task queue에 할당된 callback들이 모두 실행되고, 심지어 새로 들어온 callback까지 전부 처리될 때 까지 머물러있는다
+// 한 loop당 하나의 callback만 call stack에 전달해주는 task queue와는 대조적임
+
 function handleClick() {
     console.log('handle click');
     Promise.resolve(0)
@@ -11,5 +14,5 @@ function handleClick() {
 
 const button = document.querySelector('.btn');
 button.addEventListener('click', () => {
-    handleClick(); // 반복적으로 수행되다가 micro task queue가 꽉 차게 되면 저절로 종료
+    handleClick(); // 반복적으로 수행되다가 micro task queue가 꽉 차게 되면 종료
 });
